fa3a6d0ea4b9ef5ac808445eeb79e2011a23e0f7
Alex Ball <ball.alex.509@gmail.com>
2015-02-23 18:28:20 -0800

    added validation labels and scaler calibration
    added validation labels and scaler calibration return values to
    train_model_library

--- train_model_library.py
+++ train_model_library.py
@@ -5,6 +5,18 @@ from sklearn import preprocessing, grid_search, cross_validation
 TRAIN_PATH = 'kaggle_train_tf_idf.csv'
 
 def train_model_library(n_folds = 5, n_folds_to_compute = 5):
+    # ensemble_library_pred is an array of predictions made by the individual models.
+    # each row is an obesrvation in the validation set, and each column is the
+    # prediction of a cross validation model.
+    # validation_labels is a column vector corresponding to the labels of the
+    # observations in the validation set.
+    # model_grid is a list of lists. each element corresponds to a single model.
+    # m = model_grid[i]. m[0] is the model index (corresponding to a column in
+    # ensemble_library_pred. m[1] is a list of n_folds_to_compute model objects.
+    # m[2] is empty and holds the predictions of each model until the end
+    # m[3] is the constructor for that model i.
+    # m[4] is a dictionary specifying the model parameters for model i.
+
     ids, features, labels = util.load_train(TRAIN_PATH)
     kf_cv = cross_validation.KFold(features.shape[0], n_folds = n_folds, shuffle = True)
     scaler = preprocessing.StandardScaler()
@@ -13,26 +25,31 @@ def train_model_library(n_folds = 5, n_folds_to_compute = 5):
     
     tot_v_size = 0
     i = 1
+    validation_labels = []
     for train_idx, validate_idx in kf_cv:
         training_features = scaler.fit_transform(features[train_idx, :])
         training_labels = labels[train_idx]
         validation_features = scaler.transform(features[validate_idx, :])
-        validation_labels = labels[validate_idx]
+        validation_labels.append(labels[validate_idx][:, np.newaxis])
         _train_fold(model_grid, training_features, training_labels, validation_features)
         
         tot_v_size += validate_idx.size
         if i >= n_folds_to_compute:
             break
         i += 1
-    for 
     
+    # calibrate scaler to entire training set for subsequent testing
+    scaler.fit(features)
+    # stack individual validation folds
+    validation_labels = np.vstack(validation_labels)
+    # populate the ensemble library pred and empty the model store to reduce memory
     ensemble_library_pred = np.zeros((tot_v_size, len(model_grid)))
     for model in model_grid:
         ensemble_library_pred[:, model[0]] = np.vstack(model[2])
-    
-    return ensemble_library_pred, model_grid
-    
+        model[2] = [] 
 
+    return ensemble_library_pred, validation_labels, scaler, model_grid
+    
 def _train_fold(model_grid, training_features, training_labels, validation_features):
     for model in model_grid:
         m = model[3](**model[4])
