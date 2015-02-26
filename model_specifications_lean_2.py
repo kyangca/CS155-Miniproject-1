0c3664173b20ac5a415a6aade422b78550dd59e0
nfruitwa <nfruitwa@caltech.edu>
2015-02-25 22:42:43 -0800

    Added comments

--- model_specifications_lean_2.py
+++ model_specifications_lean_2.py
@@ -1,40 +1,48 @@
 import numpy as np
 from sklearn import * 
 
+# dictionary specifying models and parameters to include in model library
 models = {
     linear_model.LogisticRegression: {
         'parameters': {
-            'penalty' : ['l1', 'l2'],
-            'C': np.logspace(-4, 8, 13, True)
+            'penalty' : ['l1', 'l2'], # norm used in penalty term
+            'C': np.logspace(-4, 8, 13, True) # inverse regularization strength
             },
-        'dataset_type': 'standardized'
-        },
         
+        # 'standardized' means that the data set should be standardized 
+        # along each feature
+        'dataset_type': 'standardized' 
+        },
+    
+    # Classifies points using the values of its K nearest neighbors
     neighbors.KNeighborsClassifier: {
         'parameters': {
-            'n_neighbors': [1, 5, 10, 30],
-            'weights': ['uniform', 'distance']
+            'n_neighbors': [1, 5, 10, 30], # number of nearest neighbors used in classification
+            'weights': ['uniform', 'distance'] # 'distance' weights each NN by 1/distance
             },
         'dataset_type': 'standardized'
         },
-        
+    
+    # estimates likelihood by counting data
     naive_bayes.MultinomialNB: {
         'parameters': {
-            'alpha': np.append(np.linspace(0, 3, 10), 1)
+            'alpha': np.append(np.linspace(0, 3, 10), 1) # smoothing parameter 
             },
         'dataset_type': 'unstandardized'
         },
-
+    
+    # assumes Gaussian likelihood
     naive_bayes.GaussianNB : {
         'parameters': { },
         'dataset_type': 'standardized'
         },
-
+    
+    # SVM classifier
     svm.SVC: {
         'parameters': [
             {
                 'kernel': ['rbf'], 
-                'C': np.logspace(-7, 3, 11, True),  
+                'C': np.logspace(-7, 3, 11, True),  # missclassification penalty
                 'probability': [True]
                 },
         ],
